9159fbfa49d0c62007bbde86eda622cf8006527f
Mateus-Pasqueta <Mateuspasquetap@outlook.com>
2025-03-26 18:01:29 -0300

    feat: ensure cart product quantity can be decresed

--- src/app/[slug]/menu/contexts/card.tsx
+++ src/app/[slug]/menu/contexts/card.tsx
@@ -3,7 +3,8 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-export interface CardProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+export interface CardProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
@@ -12,6 +13,7 @@ export interface ICartContext {
   products: CardProduct[];
   toggleCart: () => void;
   addProduct: (product: CardProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -19,6 +21,7 @@ export const CartContext = createContext<ICartContext>({
   products: [],
   toggleCart: () => {},
   addProduct: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -48,6 +51,21 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       });
     });
   };
+
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id !== productId) {
+          return prevProduct;
+        }
+
+        if (prevProduct.quantity === 1) {
+          return prevProduct;
+        }
+        return { ...prevProduct, quantity: prevProduct.quantity - 1 };
+      });
+    });
+  };
   return (
     <CartContext.Provider
       value={{
@@ -55,6 +73,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         products,
         toggleCart,
         addProduct,
+        decreaseProductQuantity,
       }}
     >
       {children}
