f563d05050f759d829d0df8412a2e5920417ca14
Mateus-Pasqueta <Mateuspasquetap@outlook.com>
2025-03-26 18:20:29 -0300

    feat: ensure cart product can be removed

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -12,7 +12,7 @@ interface CartItemProps {
 }
 
 const CartProductItem = ({ product }: CartItemProps) => {
-  const { decreaseProductQuantity, increaseProductQuantity } = useContext(CartContext);
+  const { decreaseProductQuantity, increaseProductQuantity, removeProduct } = useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
       {/* Esquerda */}
@@ -40,7 +40,7 @@ const CartProductItem = ({ product }: CartItemProps) => {
         </div>
       </div>
       {/* Botao de delete */}
-      <Button className="h-7 w-7 rounded-lg -ml-10" variant="outline">
+      <Button className="h-7 w-7 rounded-lg -ml-10" variant="outline" onClick={() => removeProduct(product.id)}>
         <TrashIcon />
       </Button>
     </div>
